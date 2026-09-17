10969fb708d468c1d851769e95886204a8d0c8b6
Rahul Chaudhary <reshrajchaudhary009@gmail.com>
2026-09-17 11:15:57 +0545

    Resolved conflict and merged submission and project creation

--- AcademiQ Backend/src/main/java/com/softwareprojectmanagement/Repository/ProposalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.softwareprojectmanagement.Repository;
-
-import com.softwareprojectmanagement.Models.Proposal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProposalRepository extends JpaRepository<Proposal, Integer> {
-}
